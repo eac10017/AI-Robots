0cc52054ea6a731f5d1e2924d251be6458158806
eac10017 <emuc1212@gmail.com>
2026-01-13 04:00:16 -0500

    Simplify effect activation and deactivation
    Remove background and text color changes on effect activation.

--- javascript.js
+++ javascript.js
@@ -3,14 +3,10 @@ let robotImage = container.querySelector(".robot");
 
 function activateEffect() {
   robotImage.style.filter = "grayscale(100%)";
-  container.style.backgroundColor = "royalblue";
-  container.style.color = "white";
 }
 
 function deactivateEffect() {
   robotImage.style.filter = "grayscale(0%)"; 
-  container.style.backgroundColor = "aliceblue";
-  container.style.color = "black";
 }
 
 container.addEventListener("pointerover", activateEffect);
